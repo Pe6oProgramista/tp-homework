2d8f9218b9fe52c0c94633e30f3c31ff61d81431
root <root@localhost.localdomain>
2017-10-10 13:39:12 -0400

    da

--- app/controllers/sum_controller.rb
+++ app/controllers/sum_controller.rb
@@ -3,8 +3,9 @@ require "csv"
 class SumController < ApplicationController  
 def index
 	#@csv_response = `curl -F "file=@/home/elsyser/file.csv" https://row-sum.herokuapp.com/`
-	arr = CSV.parse(params["file"].read)
-	CSV.each do |row|
+	arr = CSV.parse(params["sum"].read)
+	sum = 0;
+	arr.each do |row|
 		sum += row[0]
    	end
 	render text: "zdr"
