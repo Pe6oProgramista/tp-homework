339b3e1001530c6a79ae0f87e01d29bdc68295ab
root <p.milev3@gmail.com>
2017-10-10 15:15:44 -0400

    sfsdfsdf

--- app/controllers/sum_controller.rb
+++ app/controllers/sum_controller.rb
@@ -1,13 +1,15 @@
-require "csv"
+require 'csv'
 
 class SumController < ApplicationController  
-def index
-	#@csv_response = `curl -F "file=@/home/elsyser/file.csv" https://row-sum.herokuapp.com/`
-	arr = CSV.parse(params["sum"].read)
-	sum = 0;
-	arr.each do |row|
-		sum += row[0]
-   	end
-	render text: "zdr"
-  end
+	protect_from_forgery with: :null_sessions
+	
+	def index
+		#@csv_response = `curl -F "file=@/home/elsyser/file.csv" https://row-sum.herokuapp.com/`
+		arr = CSV.parse(params["sum"].read)
+		sum = 0;
+		arr.each do |row|
+			sum += row[0]
+	   	end
+		render plain: "%.2f" % (sum).ceil	
+	end
 end
